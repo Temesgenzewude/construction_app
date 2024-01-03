113f541f26a9cd3d85205b1c2f44000a1f6d19a5
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-03 23:52:35 +0300

    add home page

--- lib/src/features/Home/presentation/widgets/project_card.dart
+++ lib/src/features/Home/presentation/widgets/project_card.dart
@@ -15,80 +15,85 @@ class ProjectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: AppSizer.getHeight(context, 150),
-          width: AppSizer.getWidth(context, 156),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(
-                color: Color(0x3F000000),
-                blurRadius: 2,
-                offset: Offset(1, 1),
-                spreadRadius: 0,
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 2,
+            offset: Offset(1, 1),
+            spreadRadius: 0,
+          ),
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 2,
+            offset: Offset(-1, -1),
+            spreadRadius: 0,
+          )
+        ],
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: AppSizer.getHeight(context, 150),
+            width: AppSizer.getWidth(context, 156),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
               ),
-              BoxShadow(
-                color: Color(0x3F000000),
-                blurRadius: 2,
-                offset: Offset(-1, -1),
-                spreadRadius: 0,
-              )
-            ],
-            color: Colors.white,
-          ),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-            child: Image.asset(
-              imageUrl,
-              fit: BoxFit.cover,
+              child: Image.asset(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: AppSizer.getWidth(context, 6),
-            vertical: AppSizer.getHeight(context, 9),
-          ),
-          child: Text(
-            projectTitle,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: AppSizer.getWidth(context, 16),
-              fontFamily: 'Montserrat',
-              fontWeight: FontWeight.w600,
-              height: 0,
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppSizer.getWidth(context, 6),
+              vertical: AppSizer.getHeight(context, 9),
             ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            left: AppSizer.getWidth(context, 6),
-            bottom: AppSizer.getHeight(context, 12),
-          ),
-          child: InkWell(
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            hoverColor: Colors.transparent,
-            onTap: () {},
-            child: const Text(
-              'Projekt ansehen',
+            child: Text(
+              projectTitle,
               style: TextStyle(
-                color: Color(0xFF041675),
-                fontSize: 12,
+                color: Colors.black,
+                fontSize: AppSizer.getWidth(context, 16),
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w600,
                 height: 0,
               ),
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.only(
+              left: AppSizer.getWidth(context, 6),
+              bottom: AppSizer.getHeight(context, 12),
+            ),
+            child: InkWell(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              hoverColor: Colors.transparent,
+              onTap: () {},
+              child: const Text(
+                'Projekt ansehen',
+                style: TextStyle(
+                  color: Color(0xFF041675),
+                  fontSize: 12,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
