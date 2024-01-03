2cf2264427260156d05e2c080464365ec1d996a4
Ketema Girma <ketema.girma@a2sv.org>
2024-01-03 19:22:57 +0300

    add custom button

---
+++ lib/src/features/onboarding/presentation/widgets/custom_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomButton extends StatelessWidget {
+  final String routeName;
+  final String buttonName;
+  final double topbottom;
+  final double leftright;
+  final Color buttonColor;
+  final Color textColor;
+
+  const CustomButton({
+    Key? key,
+    required this.routeName,
+    required this.buttonName,
+    required this.topbottom,
+    required this.leftright,
+    required this.buttonColor,
+    required this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.go('/$routeName'); // Navigate to the provided route
+      },
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        padding: EdgeInsets.only(
+          right: leftright,
+          left: leftright,
+          top: topbottom,
+          bottom: topbottom,
+        ),
+        decoration: ShapeDecoration(
+          color: buttonColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              buttonName,
+              style:  TextStyle(
+                color: textColor,
+                fontSize: 20,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
