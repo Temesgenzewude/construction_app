e1c22e197d9121f5584671793ae319e3ac2857ad
Ketema Girma <ketema.girma@a2sv.org>
2024-01-05 18:01:22 +0300

    add colors

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color.fromARGB(255, 5, 24, 118);
+  static const Color secondary = Colors.white;
+  static const Color secondaryText = Colors.black;
 }
