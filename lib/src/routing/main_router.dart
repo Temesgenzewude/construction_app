0c5a69c161a3bff69b90b69814b16834908fd505
Ketema Girma <ketema.girma@a2sv.org>
2024-01-06 15:33:17 +0300

    add Daily report screen and project report screen routing

--- lib/src/routing/main_router.dart
+++ lib/src/routing/main_router.dart
@@ -8,6 +8,7 @@ import 'package:construction_app/src/features/help/presentation/screens/help_scr
 import 'package:construction_app/src/features/onboarding/presentation/screens/begin_screen.dart';
 import 'package:construction_app/src/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:construction_app/src/features/onboarding/presentation/screens/splash_screen.dart';
+import 'package:construction_app/src/features/project/presentation/screens/daily_report_screen.dart';
 
 import 'package:construction_app/src/features/project/presentation/screens/project_report_screen.dart';
 
@@ -36,7 +37,7 @@ class _MainRouterState extends State<MainRouter> {
       errorPageBuilder: (context, state) {
         return PageNotFoundScreen();
       },
-      initialLocation: '/${AppRoutes.ConstructionScheduleScreen.name}',
+      initialLocation: '/${AppRoutes.ProjectReportScreen.name}',
       routes: [
         GoRoute(
           path: '/${AppRoutes.SplashScreen.name}',
@@ -117,6 +118,12 @@ class _MainRouterState extends State<MainRouter> {
           name: AppRoutes.ProjectReportScreen.name,
           pageBuilder: (context, state) => buildCustomTransitionForPage(
               context, state,   ProjectReportScreen()),
+        ),
+        GoRoute(
+          path: '/${AppRoutes.DailyReportScreen.name}',
+          name: AppRoutes.DailyReportScreen.name,
+          pageBuilder: (context, state) => buildCustomTransitionForPage(
+              context, state,  const DailyReportScreen()),
         ),
         GoRoute(
           path: '/${AppRoutes.DocumentsScreen.name}',
