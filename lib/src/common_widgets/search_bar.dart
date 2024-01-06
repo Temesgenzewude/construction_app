d334f5966aade35db87be4cc3a96f74c04249952
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-06 10:54:40 +0300

    add search bar widget

---
+++ lib/src/common_widgets/search_bar.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 4,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 2,
+            offset: Offset(1, 1),
+            spreadRadius: 0,
+          ),
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 2,
+            offset: Offset(-1, -1),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      width: double.infinity,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.search),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                isDense: true,
+              ),
+            ),
+          ),
+          Text('Suchen')
+        ],
+      ),
+    );
+  }
+}
