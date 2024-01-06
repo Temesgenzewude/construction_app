e1f7a7622cc91539ae22eb048496ae7fa672583c
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-06 10:54:21 +0300

    update projects screen

--- lib/src/features/project/presentation/screens/projects_screen.dart
+++ lib/src/features/project/presentation/screens/projects_screen.dart
@@ -1,3 +1,4 @@
+import 'package:construction_app/src/common_widgets/search_bar.dart';
 import 'package:construction_app/src/constants/colors.dart';
 import 'package:construction_app/src/utils/app_sizer.dart';
 import 'package:flutter/material.dart';
@@ -47,7 +48,7 @@ class ProjectsScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SearchBar(),
+              const SearchBarWidget(),
               SizedBox(
                 height: AppSizer.getHeight(context, 20),
               ),
@@ -216,57 +217,6 @@ class Card extends StatelessWidget {
             fontSize: 12,
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SearchBar extends StatelessWidget {
-  const SearchBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 4,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.white,
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x3F000000),
-            blurRadius: 2,
-            offset: Offset(1, 1),
-            spreadRadius: 0,
-          ),
-          BoxShadow(
-            color: Color(0x3F000000),
-            blurRadius: 2,
-            offset: Offset(-1, -1),
-            spreadRadius: 0,
-          )
-        ],
-      ),
-      width: double.infinity,
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(Icons.search),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                isDense: true,
-              ),
-            ),
-          ),
-          Text('Suchen')
-        ],
       ),
     );
   }
