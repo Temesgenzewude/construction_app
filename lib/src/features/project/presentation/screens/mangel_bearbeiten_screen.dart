06078fdc0b303f67dfb4cfdf1a011548f2274cb0
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-08 11:19:52 +0300

    update positoin for save button

--- lib/src/features/project/presentation/screens/mangel_bearbeiten_screen.dart
+++ lib/src/features/project/presentation/screens/mangel_bearbeiten_screen.dart
@@ -67,248 +67,250 @@ class MangelBearbeitenScreen extends HookWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppSizer.getWidth(context, 39),
-          vertical: AppSizer.getHeight(context, 22),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FieldLable(
-              labelText: 'Projektstart',
-            ),
-            CustomTextField(
-              textEditingController: nameController,
-              hintText: '',
-              isDatePicker: false,
-            ),
-            SizedBox(
-              height: AppSizer.getHeight(context, 18),
-            ),
-            FieldLable(
-              labelText: 'Projektstart',
-            ),
-            CustomTextField(
-              textEditingController: descriptionController,
-              hintText: '',
-              isDatePicker: false,
-            ),
-            SizedBox(
-              height: AppSizer.getHeight(context, 18),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FieldLable(
-                  labelText: 'Frist',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FixedTextFieldWidget(controller: fristController),
-                    IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                        'assets/images/edit.svg',
-                      ),
-                      iconSize: 18,
-                    )
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FieldLable(
-                  labelText: 'Abgestellt am',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FixedTextFieldWidget(controller: fristController),
-                    IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                        'assets/images/edit.svg',
-                      ),
-                      iconSize: 18,
-                    )
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FieldLable(
-                  labelText: 'Verursacher',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FixedTextFieldWidget(controller: fristController),
-                    IconButton(
-                      splashRadius: 0.1,
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                        'assets/images/edit.svg',
-                      ),
-                      iconSize: 18,
-                    )
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Fotos',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppSizer.getWidth(context, 39),
+            vertical: AppSizer.getHeight(context, 22),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FieldLable(
+                labelText: 'Projektstart',
+              ),
+              CustomTextField(
+                textEditingController: nameController,
+                hintText: '',
+                isDatePicker: false,
+              ),
+              SizedBox(
+                height: AppSizer.getHeight(context, 18),
+              ),
+              FieldLable(
+                labelText: 'Projektstart',
+              ),
+              CustomTextField(
+                textEditingController: descriptionController,
+                hintText: '',
+                isDatePicker: false,
+              ),
+              SizedBox(
+                height: AppSizer.getHeight(context, 18),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FieldLable(
+                    labelText: 'Frist',
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: AppSizer.getWidth(context, 16),
-                    vertical: AppSizer.getHeight(context, 15),
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(1, 1),
-                        spreadRadius: 0,
-                      ),
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(-1, -1),
-                        spreadRadius: 0,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FixedTextFieldWidget(controller: fristController),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          'assets/images/edit.svg',
+                        ),
+                        iconSize: 18,
                       )
                     ],
                   ),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.camera_alt_outlined,
-                      size: 22,
-                    ),
-                    onPressed: () {},
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FieldLable(
+                    labelText: 'Abgestellt am',
                   ),
-                ),
-                const Text(
-                  'or',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: AppSizer.getWidth(context, 16),
-                    vertical: AppSizer.getHeight(context, 16),
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(1, 1),
-                        spreadRadius: 0,
-                      ),
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(-1, -1),
-                        spreadRadius: 0,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FixedTextFieldWidget(controller: fristController),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          'assets/images/edit.svg',
+                        ),
+                        iconSize: 18,
                       )
                     ],
                   ),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.add_photo_alternate,
-                      color: AppColors.primary,
-                    ),
-                    onPressed: () {},
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FieldLable(
+                    labelText: 'Verursacher',
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: AppSizer.getHeight(context, 18),
-            ),
-            FieldLable(
-              labelText: 'Anmerkungen',
-            ),
-            CustomTextField(
-              textEditingController: fristController,
-              maxLines: 2,
-              hintText: '',
-              isDatePicker: false,
-            ),
-            SizedBox(
-              height: AppSizer.getHeight(context, 30),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(1, 1),
-                        spreadRadius: 0,
-                      ),
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 2,
-                        offset: Offset(-1, -1),
-                        spreadRadius: 0,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FixedTextFieldWidget(controller: fristController),
+                      IconButton(
+                        splashRadius: 0.1,
+                        padding: const EdgeInsets.all(0),
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          'assets/images/edit.svg',
+                        ),
+                        iconSize: 18,
                       )
                     ],
                   ),
-                  child: FloatingActionButton(
-                    onPressed: () {},
-                    shape: const CircleBorder(),
-                    backgroundColor: Colors.white,
-                    elevation: 0,
-                    child: const Icon(
-                      Icons.save_outlined,
-                      color: AppColors.primary,
-                      size: 30,
+                ],
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Fotos',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w600,
+                      height: 0,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: AppSizer.getWidth(context, 16),
+                      vertical: AppSizer.getHeight(context, 15),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(1, 1),
+                          spreadRadius: 0,
+                        ),
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(-1, -1),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.camera_alt_outlined,
+                        size: 22,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  const Text(
+                    'or',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: AppSizer.getWidth(context, 16),
+                      vertical: AppSizer.getHeight(context, 16),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(1, 1),
+                          spreadRadius: 0,
+                        ),
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(-1, -1),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.add_photo_alternate,
+                        color: AppColors.primary,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: AppSizer.getHeight(context, 18),
+              ),
+              FieldLable(
+                labelText: 'Anmerkungen',
+              ),
+              CustomTextField(
+                textEditingController: fristController,
+                maxLines: 2,
+                hintText: '',
+                isDatePicker: false,
+              ),
+              SizedBox(
+                height: AppSizer.getHeight(context, 56),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(1, 1),
+                          spreadRadius: 0,
+                        ),
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 2,
+                          offset: Offset(-1, -1),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: FloatingActionButton(
+                      onPressed: () {},
+                      shape: const CircleBorder(),
+                      backgroundColor: Colors.white,
+                      elevation: 0,
+                      child: const Icon(
+                        Icons.save_outlined,
+                        color: AppColors.primary,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
         ),
       ),
     );
