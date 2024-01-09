9ce05ab6d3dd9650a0fa016006fa8b6f5d01beb6
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-09 14:57:28 +0300

    update drawer widget

--- lib/src/features/Home/presentation/widgets/drawer_widget.dart
+++ lib/src/features/Home/presentation/widgets/drawer_widget.dart
@@ -55,7 +55,7 @@ class DrawerWidget extends StatelessWidget {
         const Icon(
           Icons.logout,
         ),
-        'Datenschutzrichtlinie',
+        'Ausloggen',
         () {}
       ],
     ];
